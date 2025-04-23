f3cd562b19bc28b12c91f15c6ced61a0fe747570
CristianCiubancan <balooprogames@gmail.com>
2025-04-23 22:34:33 +0300

    fix

--- src/plugins/[misc2]/[sub-sub-folder]/example1/html/Page.tsx
+++ src/plugins/[misc2]/[sub-sub-folder]/example1/html/Page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="h-auto min-h-56 bg-red-500 p-4 text-white flex items-end">
-      This is a test x to sasdas
+      This is a
     </div>
   );
 }
