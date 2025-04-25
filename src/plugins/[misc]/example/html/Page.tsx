c34b9c1a1ec5460985ea8b3d8740309df05c6404
CristianCiubancan <balooprogames@gmail.com>
2025-04-25 22:17:23 +0300

    cleanup

--- src/plugins/[misc]/example/html/Page.tsx
+++ src/plugins/[misc]/example/html/Page.tsx
@@ -1,3 +1,25 @@
+import { useCallback, useState } from 'react';
+import { useNuiEvent } from '../../../../webview/hooks/useNuiEvent';
+import { fetchNui } from '../../../../utils/fetchNui';
+
 export default function Page() {
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  useNuiEvent('example:toggle-ui', (data) => {
+    if (data) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  });
+
+  const handleCloseUi = useCallback(async () => {
+    await fetchNui('example:toggle-ui', false);
+  }, [fetchNui]);
+
+  return isOpen ? (
+    <div>
+      <button onClick={handleCloseUi}>close</button>
+    </div>
+  ) : null;
 }
