6b0ab090e038b603007b7a21bd34711006b5c066
CristianCiubancan <balooprogames@gmail.com>
2025-04-25 22:34:29 +0300

    fixes

--- src/plugins/[character]/character-edit/client/index.ts
+++
@@ -1,6 +0,0 @@
-console.log(
-  'Example2 client script loaded! - Testing DRY resource reload notification'
-);
-console.log(
-  'This is a test of the resource reloading system - Testing DRY resource reload notification'
-);
