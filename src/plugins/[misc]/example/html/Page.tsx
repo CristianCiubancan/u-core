50c1e7975ce7ca82a6dc766b5d59ba1e36b1f614
CristianCiubancan <balooprogames@gmail.com>
2025-04-25 21:34:49 +0300

    top

--- src/plugins/[misc]/example/html/Page.tsx
+++ src/plugins/[misc]/example/html/Page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <div className="h-64"> Cacat 123123</div>;
+  return (
+    <div className="h-screen flex items-end bg-indigo-950 text-white z-10">
+      Cacat
+    </div>
+  );
 }
