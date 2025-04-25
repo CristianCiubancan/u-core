b260a822297df80d155a2c1861bea00a09cf5b7d
CristianCiubancan <balooprogames@gmail.com>
2025-04-25 21:34:49 +0300

    top

--- src/plugins/[misc2]/example3/html/Page.tsx
+++ src/plugins/[misc2]/example3/html/Page.tsx
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-    <div className="h-auto min-h-32 bg-red-500 p-4 text-white">
-      <h2 className="font-bold">Message Receiver</h2>
-    </div>
-  );
+  return <div>Message Receiver</div>;
 }
