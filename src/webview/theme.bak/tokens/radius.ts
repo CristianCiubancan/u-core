96b3c56f853c60900b248fc3c2179dd6cecfde88
CristianCiubancan <balooprogames@gmail.com>
2025-04-27 16:17:31 +0300

    cleanup

--- src/webview/theme.bak/tokens/radius.ts
+++
@@ -1,8 +0,0 @@
-// src/webview/theme/tokens/radius.ts
-
-import { borders } from './constants';
-
-/**
- * Border radius tokens for consistent rounding of UI elements
- */
-export const borderRadius = borders.radius;
